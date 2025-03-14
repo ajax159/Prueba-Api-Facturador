95f58286eff973e49473222c8b5b730f65ff6734
ajax159 <gmod159@gmail.com>
2025-03-14 11:02:35 -0500

    First

---
+++ src/main/java/com/facturador/test/TestFacturadorApplication.java
@@ -0,0 +1,13 @@
+package com.facturador.test;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestFacturadorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TestFacturadorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/facturador/test/TestFacturadorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.facturador.test;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestFacturadorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
